5af87dca000d04ec4a01f19f779c347d2767ac87
Tristan Cavelier <t.cavelier@free.fr>
2016-09-28 17:01:19 +0200

    +1

--- 1a/env.unicode.js
+++ 1a/env.unicode.js
@@ -12,6 +12,7 @@
       http://www.wtfpl.net/ for more details. */
 
   // provides: env.unicodeToText,
+  //           env.decodeUtf8BytesToText,
   //           env.softDecodeUtf8BytesToText,
   //           env.encodeTextToUtf8Bytes,
   //           env.softEncodeTextToUtf8Bytes
@@ -39,29 +40,75 @@
 
   // https://en.wikipedia.org/wiki/UTF-8
   // http://stackoverflow.com/questions/13235091/extract-the-first-letter-of-a-utf-8-string-with-lua#13238257
-  env.softDecodeUtf8BytesToText = function (ba) {
+  env.decodeUtf8BytesToText = function (ba) {
     // XXX http://www.unicode.org/faq/utf_bom.html
-    var bai = 0, bal = ba.length,
-        ci, cl, count,
-        code, codes = [];
+    var bai = 0, bal = ba.length, ci, code, codes = [];
     while (bai < bal) {
       code = ba[bai];
-      if (0x00 <= code && code <= 0x7F || 0xC2 <= code && code <= 0xF4) {
-        for (count = 1, ci = bai + 1, cl = bai + 4; ci < cl && ci < bal; ci += 1)
-          if (0x80 <= ba[ci] && ba[ci] <= 0xBF) {
-            count += 1;
-            code = (code << 6) | (ba[ci] & 0x3F);
-          }
-        switch (count) {
-          case 1: codes.push(code); bai += 1; break;
-          case 2: codes.push(code & 0x7FF); bai += 2; break;
-          case 3: codes.push(code & 0xFFFF); bai += 3; break;
-          case 4: codes.push(code & 0x1FFFFF); bai += 4; break;
-          default: throw new Error("should not happen");
+      if (code <= 0x7F) {
+        codes.push(code);
+        bai += 1;
+      } else if ((0xE0 & code) === 0xC0) {
+        if (code >= 0xC2 && (ci = bai + 1) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533) {
+          codes.push(code & 0x7FF);
+          bai += 2;
+        } else throw new Error("invalid UTF-8 byte sequence");
+      } else if ((0xF0 & code) === 0xE0) {
+        if ((ci = bai + 1) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533 &&
+            (ci = bai + 2) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533) {
+          codes.push(code & 0xFFFF);
+          bai += 3;
+        } else throw new Error("invalid UTF-8 byte sequence");
+      } else if ((0xF8 & code) === 0xF0) {
+        if ((ci = bai + 1) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533 &&
+            (ci = bai + 2) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533 &&
+            (ci = bai + 3) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533) {
+          codes.push(code & 0x1FFFFF);
+          bai += 4;
+        } else throw new Error("invalid UTF-8 byte sequence");
+      } else throw new Error("invalid UTF-8 byte sequence");
+    }
+    return env.unicodeToText.apply(env, codes);
+  };
+
+  env.softDecodeUtf8BytesToText = function (ba) {
+    // XXX http://www.unicode.org/faq/utf_bom.html
+    var bai = 0, bal = ba.length, ci, code, codes = [];
+    while (bai < bal) {
+      code = ba[bai];
+      if (code <= 0x7F) {
+        codes.push(code);
+        bai += 1;
+      } else if ((0xE0 & code) === 0xC0) {
+        if (code >= 0xC2 && (ci = bai + 1) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533) {
+          codes.push(code & 0x7FF);
+          bai += 2;
+        } else {
+          codes.push(65533);
+          bai += 1;
+        }
+      } else if ((0xF0 & code) === 0xE0) {
+        if ((ci = bai + 1) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533 &&
+            (ci = bai + 2) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533) {
+          codes.push(code & 0xFFFF);
+          bai += 3;
+        } else {
+          codes.push(65533);
+          bai += 1;
+        }
+      } else if ((0xF8 & code) === 0xF0) {
+        if ((ci = bai + 1) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533 &&
+            (ci = bai + 2) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533 &&
+            (ci = bai + 3) < bal && (code = ((0xC0 & ba[ci]) === 0x80 ? (code << 6) | (ba[ci] & 0x3F) : 65533)) !== 65533) {
+          codes.push(code & 0x1FFFFF);
+          bai += 4;
+        } else {
+          codes.push(65533);
+          bai += 1;
         }
       } else {
+        codes.push(65533);
         bai += 1;
-        codes.push(code);
       }
     }
     return env.unicodeToText.apply(env, codes);
@@ -77,7 +124,7 @@
         a.push(c);
       } else if (c < 0x800) {
         a.push((c >> 6) | 0xc0, (c & 0x3f) | 0x80);
-      } else if ((c >= 0xd800) && (c <= 0xdbff)) {
+      } else if (0xd800 <= c && c <= 0xdbff) {
         n += 1;
         if (n >= s.length)
           throw new Error("invalid string");  // incomplete surrogate pair
@@ -91,6 +138,8 @@
           ((c >> 6) & 0x3f) | 0x80,
           (c & 0x3f) | 0x80
         );
+      } else if (0xdc00 <= c && c <= 0xdfff) {
+        throw new Error("invalid string");
       } else {
         a.push(
           (c >> 12) | 0xe0,
@@ -112,7 +161,7 @@
         a.push(c);
       } else if (c < 0x800) {
         a.push((c >> 6) | 0xc0, (c & 0x3f) | 0x80);
-      } else if ((c >= 0xd800) && (c <= 0xdbff)) {
+      } else if (0xd800 <= c && c <= 0xdbff) {
         if (n + 1 >= s.length) {
           a.push(0xef, 0xbf, 0xbd);  // incomplete surrogate pair
           break;
@@ -130,6 +179,8 @@
             (c & 0x3f) | 0x80
           );
         }
+      } else if (0xdc00 <= c && c <= 0xdfff) {
+        a.push(0xef, 0xbf, 0xbd);
       } else {
         a.push(
           (c >> 12) | 0xe0,
--- 1a/env.unicode.tests.js
+++ 1a/env.unicode.tests.js
@@ -33,29 +33,53 @@
     });
   }
 
-  function readBlobAsText(blob, callback) {
+  function readBlobAs(as, blob, callback) {
     /*global FileReader */
     var fr = new FileReader();
     fr.onload = function (e) { return callback(undefined, e.target.result); };
     fr.onerror = function () { return callback(new Error("unable to read blob as text")); };
     fr.onabort = function () { return callback(new Error("cancelled")); };
-    fr.readAsText(blob);
+    fr["readAs" + as](blob);
   }
   function nativeSoftDecodeUtf8BytesToText(bytes, callback) {
-    readBlobAsText(new Blob([new Uint8Array(bytes).buffer]), callback);
+    readBlobAs("Text", new Blob([new Uint8Array(bytes).buffer]), callback);
   }
+  function nativeSoftEncodeTextToUtf8Bytes(text, callback) {
+    readBlobAs("ArrayBuffer", new Blob([text]), callback);
+  }
+
   function bytesToJs(bytes) {
     return "[" + bytes.map(function (v) { return "0x" + v.toString(16); }).join(",") + "]";
   }
+  function textToShorts(text) {
+    var i = 0, r = [];
+    for (; i < text.length; i += 1) r.push(text.charCodeAt(i));
+    return r;
+  }
+
   function randBytes(length) {
     var r = [], i = 0;
     while (i++ < length) r.push(parseInt(Math.random() * 256, 10));
     return r;
   }
+  function randText(length) {
+    var r = [], i = 0;
+    while (i++ < length) r.push(parseInt(Math.random() * 65536, 10));
+    return String.fromCharCode.apply(String, r);
+  }
+
   function testSoftDecodeUtf8BytesToText(bytes) {
     nativeSoftDecodeUtf8BytesToText(bytes, function (err, text) {
       test("unicode " + bytesToJs(bytes), 300, [text], function (res, end) {
         res.push(env.softDecodeUtf8BytesToText(bytes));
+        end();
+      });
+    });
+  }
+  function testSoftEncodeTextToUtf8Bytes(text) {
+    nativeSoftEncodeTextToUtf8Bytes(text, function (err, bytes) {
+      test("unicode " + JSON.stringify(text) + " " + bytesToJs(textToShorts(text)), 300, Array.from(new Uint8Array(bytes)), function (res, end) {
+        res.push.apply(res, env.softEncodeTextToUtf8Bytes(text));
         end();
       });
     });
@@ -67,12 +91,23 @@
   testSoftDecodeUtf8BytesToText([0xC3, 0xA9]);  // &eacute;
   testSoftDecodeUtf8BytesToText([0xC2, 0x80]);  // String.fromCharCode(128);
   testSoftDecodeUtf8BytesToText([0xA9]);  // String.fromCharCode(65533);
-  testSoftDecodeUtf8BytesToText(randBytes(1));
-  testSoftDecodeUtf8BytesToText(randBytes(2));
-  testSoftDecodeUtf8BytesToText(randBytes(3));
-  testSoftDecodeUtf8BytesToText(randBytes(4));
+  testSoftDecodeUtf8BytesToText([0xE8]);
+  testSoftDecodeUtf8BytesToText([0xD8, 0x37, 0x22]);  // "\uFFFF7\""
+  testSoftDecodeUtf8BytesToText([0xC3, 0xA9, 0xA9, 0xA9]);
+  testSoftDecodeUtf8BytesToText([0xc0,0x9d]);
+  testSoftDecodeUtf8BytesToText([0xc1,0x98]);
+  //testSoftDecodeUtf8BytesToText(randBytes(1));
+  //testSoftDecodeUtf8BytesToText(randBytes(2));
+  //testSoftDecodeUtf8BytesToText(randBytes(3));
+  //testSoftDecodeUtf8BytesToText(randBytes(4));
   testSoftDecodeUtf8BytesToText(randBytes(Math.random() * 100));
 
-  // XXX test softEncodeTextToUtf8Bytes
+  testSoftEncodeTextToUtf8Bytes("a");
+  testSoftEncodeTextToUtf8Bytes("eacute is é");
+  testSoftEncodeTextToUtf8Bytes("日本語");
+  testSoftEncodeTextToUtf8Bytes("\udfff");
+  testSoftEncodeTextToUtf8Bytes("\ue000");
+  testSoftEncodeTextToUtf8Bytes(String.fromCharCode(0x5359,0xdc39));
+  testSoftEncodeTextToUtf8Bytes(randText(Math.random() * 100));
 
 }(this.env));
