af1ddae662c1afb4e63f3367eadd5ce93832acd9
Tristan Cavelier <t.cavelier@free.fr>
2016-10-21 11:28:02 +0200

    +1

--- 1a/tc-textarea-keymap.js
+++ 1a/tc-textarea-keymap.js
@@ -8,90 +8,6 @@
       To Public License, Version 2, as published by Sam Hocevar. See
       http://www.wtfpl.net/ for more details. */
 
-  // BEGIN useless
-  var keyCodeStr = {
-    8: "Backspace",
-    9: "Tab",
-    13: "Enter",
-    16: "Shift",
-    17: "Control",
-    18: "Alt",
-    19: "Pause",
-    20: "CapsLock",
-    27: "Escape",
-    32: "Space",
-    33: "PageUp",
-    34: "PageDown",
-    35: "End",
-    36: "Home",
-    37: "ArrowLeft",
-    38: "ArrowUp",
-    39: "ArrowRight",
-    40: "ArrowDown",
-    45: "Insert",
-    46: "Delete",
-    //48: "0",
-    //49: "1",
-    //50: "2",
-    //51: "3",
-    //52: "4",
-    //53: "5",
-    //54: "6",
-    //55: "7",
-    //56: "8",
-    //57: "9",
-    //65: "A",
-    //66: "B",
-    //67: "C",
-    //68: "D",
-    //69: "E",
-    //70: "F",
-    //71: "G",
-    //72: "H",
-    //73: "I",
-    //74: "J",
-    //75: "K",
-    //76: "L",
-    //77: "M",
-    //78: "N",
-    //79: "O",
-    //80: "P",
-    //81: "Q",
-    //82: "R",
-    //83: "S",
-    //84: "T",
-    //85: "U",
-    //86: "V",
-    //87: "W",
-    //88: "X",
-    //89: "Y",
-    //90: "Z",
-    91: "OS",
-    93: "ContextMenu",
-    112: "F1",
-    113: "F2",
-    114: "F3",
-    115: "F4",
-    116: "F5",
-    117: "F6",
-    118: "F7",
-    119: "F8",
-    120: "F9",
-    121: "F10",
-    122: "F11",
-    123: "F12",
-    124: "F13",
-    125: "F14",
-    126: "F15",
-    127: "F16",
-    176: "MediaTrackNext",
-    177: "MediaTrackPrevious",
-    178: "MediaStop",
-    179: "MediaPlayPause",
-    225: "AltGraph",
-  };
-  // END useless
-
   function parseSedS(string) {
     // parseSedS("/hello/lol/g") -> [/hello/g, "lol"]
     var res = /^\/((?:\\.|[^\\\/])*)\/((?:[^/]|\\\/)*)\/([gimyu]{0,5})$/.exec(string);  // this regexp does not handle flag errors!
@@ -899,10 +815,12 @@
   shortcuts["Alt-;"] = function (event) {
     event.preventDefault();
     promptCommand(event.target);
+    if (event.target.focused) { event.target.blur(); event.target.focus(); }  // XXX needed in chrome
   };
   shortcuts["Alt-R"] = function (event) {
     event.preventDefault();
     if (event.target["[[TcTextareaKeymapLastAction]]"]) event.target["[[TcTextareaKeymapLastAction]]"]();
+    if (event.target.focused) { event.target.blur(); event.target.focus(); }  // XXX needed in chrome
   };
 
   //textarea.oninput = (e) => console.log(e.target.value);
