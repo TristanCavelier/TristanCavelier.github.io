fb4b200bc18c044d68571be6a83824e767d40ce9
Tristan Cavelier <t.cavelier@free.fr>
2016-11-04 01:25:32 +0100

    +1

--- 1a/env.base64.js
+++ 1a/env.base64.js
@@ -65,9 +65,12 @@
     //   bytes to decode
     // bytes = []
     //   where decoded bytes will be written
-    // cache = []
+    // cache = [0, 0, 0, 0, 0]
     //   used by the algorithm
-    // schemeCodeMap = {
+    // schemeCodeMap = {  // the algorithm assumes it perfect
+    //     // X: 0-63 char to 6bit values
+    //     // X: 64 padding char
+    //     // X: >=65 chars to ignore
     //     A:0,B:1,C:2,D:3,E:4,F:5,G:6,H:7,I:8,J:9,K:10,L:11,M:12,
     //     N:13,O:14,P:15,Q:16,R:17,S:18,T:19,U:20,V:21,W:22,X:23,Y:24,Z:25,
     //     a:26,b:27,c:28,d:29,e:30,f:31,g:32,h:33,i:34,j:35,k:36,l:37,m:38,
@@ -75,38 +78,55 @@
     //     0:52,1:53,2:54,3:55,4:56,5:57,6:58,7:59,8:60,9:61,
     //     "+":62,"/":63,  // standard way
     //     //"-":62,"_":63,  // url way
-    //     "=":64,  // padding value
-    //     " ":65,"\t":66,"\r":67,"\n":68  // ignored values
+    //     "=":64,
+    //     " ":65,"\t":66,"\r":67,"\n":68
     //   }
-    // o.invalidByteError({codes, bytes, index}) - called on invalid byte
-    // o.unexpectedPaddingError({codes, bytes, index}) - called on unexpected padding
-    // o.unexpectedEndOfDataError({codes, bytes, index}) - called on unexpected end of data
+    // o.invalidByteError({codes, bytes, cache, index}) - called on invalid byte
+    // o.expectedPaddingError({codes, bytes, cache, index}) - called on expected padding
+    // o.unexpectedPaddingError({codes, bytes, cache, index}) - called on unexpected padding
+    // o.unexpectedEndOfDataError({codes, bytes, cache, schemeCodeMap, index, requiredCodeAmount, requiredCodeIndex, lastCodes}) - called on unexpected end of data
     // close = false (optional)
     // returns bytes of decoded base64
-    var i = 0, l = codes.length, code, a, b, c;
+
+    var i = 0, l = codes.length, code, byte, a, b, c;
     for (; i < l; i += 1) {
-      code = schemeCodeMap[codes[i]];
-      if (!(code >= 0)) o.invalidByteError({codes: codes, bytes: bytes, index: i});
-      else if (code === 64 && (cache.length % 4) <= 1) o.unexpectedPaddingError({codes: codes, bytes: bytes, index: i});
-      else if (code >= 65) {}
-      else cache.push(code);
-    }
-    if ((l = cache.length) >= 4) {
-      for (i = 0, l -= l % 4; i < l; i += 4) {
-        a = ((cache[0] << 2) & 0xFC) | ((cache[1] >>> 4) & 0x03);
-        if (cache[2] === 64) bytes.push(a);
-        else {
-          b = ((cache[1] << 4) & 0xF0) | ((cache[2] >>> 2) & 0x0F);
-          if (cache[3] === 64) bytes.push(a, b);
-          else {
-            c = ((cache[2] << 6) & 0xC0) | (cache[3] & 0x3F);
-            bytes.push(a, b, c);
-          }
-        }
-        cache.splice(0, 4);
+      code = schemeCodeMap[byte = codes[i]];
+      if (!(code >= 0)) o.invalidByteError({codes: codes, bytes: bytes, cache: cache, index: i});
+      else switch (cache[0]) {
+        case 1:  // after first byte state
+          if (code === 64) o.unexpectedPaddingError({codes: codes, bytes: bytes, cache: cache, index: i});
+          else if (code >= 65) {}
+          else { bytes.push(((cache[1] << 2) & 0xFC) | ((code >>> 4) & 0x03)); cache[3] = byte; cache[1] = code; cache[0] = 2; }
+          break;
+        case 2:  // after second byte state
+          if (code === 64) { cache[4] = byte; cache[0] = 4; }
+          else if (code >= 65) {}
+          else { bytes.push(((cache[1] << 4) & 0xF0) | ((code >>> 2) & 0x0F)); cache[4] = byte; cache[1] = code; cache[0] = 3; }
+          break;
+        case 3:  // after third byte state
+          if (code === 64) { cache[4] = byte; cache[0] = 0; }
+          else if (code >= 65) {}
+          else { bytes.push(((cache[1] << 6) & 0xC0) | (code & 0x3F)); cache[0] = 0; }
+          break;
+        case 4:  // expect padding state
+          if (code === 64) cache[0] = 0;
+          else if (code >= 65) {}
+          else o.expectedPaddingError({codes: codes, bytes: bytes, cache: cache, schemeCodeMap: schemeCodeMap, index: i});
+          break;
+        default:
+          if (code === 64) o.unexpectedPaddingError({codes: codes, bytes: bytes, cache: cache, schemeCodeMap: schemeCodeMap, index: i});
+          else if (code >= 65) {}
+          else { cache[2] = byte; cache[1] = code; cache[0] = 1; }
       }
     }
-    if (close && cache.length) o.unexpectedEndOfDataError({codes: codes, bytes: bytes, index: codes.length});
+    if (close) {
+      switch (cache[0]) {
+        case 1: o.unexpectedEndOfDataError({codes: codes, bytes: bytes, cache: cache, schemeCodeMap: schemeCodeMap, index: codes.length, requiredCodeAmount: 4, requiredCodeIndex: 1, lastCodes: [cache[2]]}); break;
+        case 2: o.unexpectedEndOfDataError({codes: codes, bytes: bytes, cache: cache, schemeCodeMap: schemeCodeMap, index: codes.length, requiredCodeAmount: 4, requiredCodeIndex: 2, lastCodes: [cache[2], cache[3]]}); break;
+        case 3:
+        case 4: o.unexpectedEndOfDataError({codes: codes, bytes: bytes, cache: cache, schemeCodeMap: schemeCodeMap, index: codes.length, requiredCodeAmount: 4, requiredCodeIndex: 3, lastCodes: [cache[2], cache[3], cache[4]]}); break;
+      }
+    }
     return bytes;
   }
   env.decodeBase64ChunkAlgorithm = decodeBase64ChunkAlgorithm;
@@ -125,8 +145,13 @@
       // built by `parseBase64SchemeForDecoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/= \t\r\n")`
     }, {
       invalidByteError: function (v) { var e = new Error("invalid byte"); e.index = v.index; throw e; },
+      expectedPaddingError: function (o) { var e = new Error("expected padding"); e.index = o.index; throw e; },
       unexpectedPaddingError: function (v) { var e = new Error("unexpected padding"); e.index = v.index; throw e; },
-      unexpectedEndOfDataError: function () { throw new Error("unexpected end of data"); }
+      unexpectedEndOfDataError: function (o) {
+        if (o.lastCodes.length < 2)
+          throw new Error("unexpected end of data");
+        o.cache.splice(0);
+      }
     }, true);
   }
   env.decodeBase64 = decodeBase64;
@@ -145,8 +170,13 @@
       // built by `parseBase64SchemeForStringDecoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/= \t\r\n")`
     }, {
       invalidByteError: function (v) { var e = new Error("invalid byte"); e.index = v.index; throw e; },
+      expectedPaddingError: function (o) { var e = new Error("expected padding"); e.index = o.index; throw e; },
       unexpectedPaddingError: function (v) { var e = new Error("unexpected padding"); e.index = v.index; throw e; },
-      unexpectedEndOfDataError: function () { throw new Error("unexpected end of data"); }
+      unexpectedEndOfDataError: function (o) {
+        if (o.lastCodes.length < 2)
+          throw new Error("unexpected end of data");
+        o.cache.splice(0);
+      }
     }, true);
   }
   env.decodeBase64String = decodeBase64String;
@@ -181,15 +211,20 @@
     e.index = o.index;
     throw e;
   };
+  Base64DecoderIo.prototype.expectedPaddingError = function (o) {
+    var e = new Error("expected padding");
+    e.index = o.index;
+    throw e;
+  };
   Base64DecoderIo.prototype.unexpectedPaddingError = function (o) {
     var e = new Error("unexpected padding");
     e.index = o.index;
     throw e;
   };
   Base64DecoderIo.prototype.unexpectedEndOfDataError = function (o) {
-    var e = new Error("unexpected end of data");
-    e.index = o.index;
-    throw e;
+    if (o.lastCodes.length < 2)
+      throw new Error("unexpected end of data");
+    o.cache.splice(0);
   };
   env.Base64DecoderIo = Base64DecoderIo;
   env.newBase64DecoderIo = function () { var c = env.Base64DecoderIo, o = Object.create(c.prototype); c.apply(o, arguments); return o; };
@@ -222,9 +257,9 @@
     //   array of byte numbers
     // codes = []
     //   where encoded codes will be written
-    // cache = []
+    // cache = [0, 0]
     //   used by the algorithm
-    // schemeCodes = [
+    // schemeCodes = [  // the algorithm assumes it perfect
     //     65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,  // A-Z
     //     97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122, // a-z
     //     48,49,50,51,52,53,54,55,56,57,  // 0-9
@@ -235,47 +270,34 @@
     // returns bytes of encoded base64
     var i = 0, l = bytes.length;
     for (; i < l; i += 1) {
-      switch (cache.length % 4) {
-        case 0:
-          cache[0] = bytes[i] >>> 2;
-          cache[1] = (bytes[i] << 4) & 0x30;
-          break;
-        case 1: cache[1] = 0;
+      switch (cache[0]) {
         case 2:
-          cache[1] |= bytes[i] >>> 4;
-          cache[2] = (bytes[i] << 2) & 0x3C;
+          codes.push(schemeCodes[cache[1] | (bytes[i] >>> 4)]);
+          cache[1] = (bytes[i] << 2) & 0x3C;
+          cache[0] = 3;
           break;
-        default:
+        case 3:
           codes.push(
-            schemeCodes[cache[0]],
-            schemeCodes[cache[1]],
-            schemeCodes[cache[2] | (bytes[i] >>> 6)],
+            schemeCodes[cache[1] | (bytes[i] >>> 6)],
             schemeCodes[bytes[i] & 0x3F]
           );
-          cache.splice(0, 3);
+          cache[0] = 0;
+          break;
+        default:
+          codes.push(schemeCodes[bytes[i] >>> 2]);
+          cache[1] = (bytes[i] << 4) & 0x30;
+          cache[0] = 2;
       }
     }
     if (close) {
-      switch (cache.length % 4) {
-        case 0: break;
-        case 1: cache[1] = 0;
+      switch (cache[0]) {
         case 2:
-          codes.push(
-            schemeCodes[cache[0]],
-            schemeCodes[cache[1]],
-            schemeCodes[64],
-            schemeCodes[64]
-          );
-          cache.splice(0, 2);
+          codes.push(schemeCodes[cache[1]], schemeCodes[64], schemeCodes[64]);
+          cache[0] = 0;
           break;
-        default:
-          codes.push(
-            schemeCodes[cache[0]],
-            schemeCodes[cache[1]],
-            schemeCodes[cache[2]],
-            schemeCodes[64]
-          );
-          cache.splice(0, 3);
+        case 3:
+          codes.push(schemeCodes[cache[1]], schemeCodes[64]);
+          cache[0] = 0;
       }
     }
     return codes;
@@ -283,7 +305,7 @@
   env.encodeBase64ChunkAlgorithm = encodeBase64ChunkAlgorithm;
 
   function encodeBase64(bytes) {
-    return env.encodeBase64ChunkAlgorithm(bytes, [], [], [
+    return env.encodeBase64ChunkAlgorithm(bytes, [], [0, 0], [
       65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,  // A-Z
       97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122, // a-z
       48,49,50,51,52,53,54,55,56,57,  // 0-9
@@ -296,13 +318,13 @@
   env.encodeBase64 = encodeBase64;
 
   function encodeBase64ToString(bytes) {
-    return env.encodeBase64ChunkAlgorithm(bytes, [], [], "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", true).join("");
+    return env.encodeBase64ChunkAlgorithm(bytes, [], [0, 0], "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", true).join("");
   }
   env.encodeBase64ToString = encodeBase64ToString;
 
   function Base64EncoderIo(scheme) {
     // scheme = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" (optional)
-    this.cache = [];
+    this.cache = [0, 0];
     this.value = [];
     if (scheme) this.schemeCodes = env.parseBase64SchemeForEncoding(scheme);
   }
