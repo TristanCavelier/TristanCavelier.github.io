1b0ac66cdd77a04cc420eef2224c45f8e5c69c58
Tristan Cavelier <t.cavelier@free.fr>
2016-12-10 19:08:55 +0100

    env.channels.js 1.0.2

--- env.channels.js
+++ env.channels.js
@@ -2,7 +2,7 @@
 (function envChannels(env) {
   "use strict";
 
-  /*! env.channels.js Version 1.0.1
+  /*! env.channels.js Version 1.0.2
 
       Copyright (c) 2015-2016 Tristan Cavelier <t.cavelier@free.fr>
       This program is free software. It comes without any warranty, to
@@ -11,160 +11,141 @@
       To Public License, Version 2, as published by Sam Hocevar. See
       http://www.wtfpl.net/ for more details. */
 
-  // dependencies: async (env.newPromise)
-  // provides: env.{,new}Channel
-  // - A Channel is like a `chan` in go.
+  // dependencies:
+  //   env.newPromise (async)
+  // provides:
+  //   env.Channel
+  //   env.newChannel
 
   if (env.registerLib) env.registerLib(envChannels);
 
-  var wm = typeof WeakMap === "function" ? new WeakMap() : {get: function (a) { return a; }, set: function () { return; }};
-
-  function channelDeferred(v, channel, key) {
-    var d = {
-      value: v,
-      channel: channel,
-      channelKey: key,
-      resolve: null,
-      reject: null,
-      promise: null
-    };
-    d.promise = env.newPromise(function (r, j) {
-      d.resolve = r;
-      d.reject = j;
-    });
-    d.promise.cancel = function () {
-      delete d.channel[d.channelKey];
-      delete d.value;
-      d.done = true;
-      d.reject(new Error("cancelled"));
-    };
-    return d;
-  }
-
-  function channelFifoPush(channel, type, value) {
-    var hik = "[[Channel:" + type + ":headIndex]]",
-      hi = channel[hik] || 0,
-      lk = "[[Channel:" + type + ":length]]",
-      hk = "[[Channel:" + type + ":" + hi + "]]",
-      deferred = channelDeferred(value, channel, hk);
-    channel[hk] = deferred;
-    channel[hik] = hi + 1;
-    channel[lk] = (channel[lk] || 0) + 1;
-    return deferred;
-  }
-  function channelFifoPop(channel, type) {
-    var v, tik = "[[Channel:" + type + ":tailIndex]]",
-      ti = channel[tik] || 0,
-      hi = channel["[[Channel:" + type + ":headIndex]]"] || 0,
-      vk = "[[Channel:" + type + ":" + ti + "]]";
-    if (ti < hi) {
-      v = channel[vk];
-      delete channel[vk];
-      channel[tik] = ti + 1;
-      channel["[[Channel:" + type + ":length]]"] -= 1;
-      return v;
-    }
-  }
-
   function Channel(capacity) {
-    // API stability level: 1 - Experimental
-    wm.set(this, {});
-    var chan = wm.get(this);
-    if (capacity > 0) chan["[[ChannelCapacity]]"] = capacity;
+    // tips:
+    //   Use as synchronous channel send and next
+    //     chan = new Channel(Infinity)
+    //     chan.send(1);  // adds 1 to the channel
+    //     chan.next(true);  // returns {value: 1}
+    //     chan.next(true);  // returns null
+    //   Use as semaphore of length 2
+    //     sem = new Channel(2);
+    //     await sem.send(1);  // acquire
+    //     await sem.next();  // release
+    this["[[ChannelPendingSend]]"] = [];
+    this["[[ChannelPendingNext]]"] = [];
+    if (capacity > 0) this["[[ChannelCapacity]]"] = +capacity;
   }
-  var CLOSED_ERROR = Channel.CLOSED_ERROR = Channel.prototype.CLOSED_ERROR = new Error("closed channel");
-  Channel.prototype.getLength = function () { return wm.get(this)["[[Channel:send:length]]"] || 0; };
-  Channel.prototype.getCapacity = function () { return wm.get(this)["[[ChannelCapacity]]"] || 0; };
+  Channel.prototype["[[ChannelPendingSend]]"] = null;
+  Channel.prototype["[[ChannelPendingNext]]"] = null;
+  Channel.prototype["[[ChannelClosed]]"] = false;
+  Channel.prototype["[[ChannelLength]]"] = 0;
+  Channel.prototype["[[ChannelCapacity]]"] = 0;
+  Channel._magic = function (a, value) {
+    var node = {value: value, promise: null, resolve: null}, reject;
+    node.promise = env.newPromise(function (r, j) { node.resolve = r; reject = j; });
+    node.promise.cancel = function () {
+      for (var l = a.length - 1; l >= 0; --l) if (a[l] === node) { a.splice(l, 1); break; }
+      reject(new Error("cancelled"));
+    };
+    return node;
+  };
+  Channel.prototype.getLength = function () { return this["[[ChannelLength]]"]; };
+  Channel.prototype.getCapacity = function () { return this["[[ChannelCapacity]]"]; };
+  Channel.prototype.isClosed = function () { return this["[[ChannelClosed]]"]; };
   Channel.prototype.close = function () {
-    /*jslint ass: true */
-    var chan = wm.get(this), next;
-    chan["[[ChannelError]]"] = CLOSED_ERROR;  // use Channel.CLOSED_ERROR ?
-    while (chan["[[Channel:next:length]]"] > 0) {
-      next = channelFifoPop(chan, "next");
-      if (next && !next.done) return next.resolve({value: undefined, done: true});
-    }
+    var n = this["[[ChannelPendingNext]]"].splice(0), i = 0, l = n.length;
+    this["[[ChannelClosed]]"] = true;
+    for (; i < l; ++i) n[i].resolve({value: undefined, done: true});
   };
-  Channel.prototype.throw = function (e) {
-    /*jslint ass: true */
-    var chan = wm.get(this), next;
-    chan["[[ChannelError]]"] = e;
-    while (chan["[[Channel:next:length]]"] > 0) {
-      next = channelFifoPop(chan, "next");
-      if (next && !next.done) return next.reject(e);
-    }
+  Channel.prototype.send = function (value, iffree) {
+    // value = ...
+    //   the value to send into the channel
+    // iffree = false
+    //   if true, send only if someone is waiting for next or if the channel is free
+    // returns boolean or Promise<boolean>, boolean is true if someone was waiting for next or if channel was free
+    var n;
+    if (this["[[ChannelClosed]]"]) throw new Error("channel closed");
+    n = this["[[ChannelPendingNext]]"].shift();
+    if (n) { n.resolve({value: value, done: false}); return true; }
+    if (this["[[ChannelLength]]"] < this["[[ChannelCapacity]]"]) { ++this["[[ChannelLength]]"]; this["[[ChannelPendingSend]]"].push({value: value}); return true; }
+    if (iffree) return false;
+    this["[[ChannelPendingSend]]"].push(n = env.Channel._magic(this["[[ChannelPendingSend]]"], value));
+    return n.promise;
   };
-  Channel.prototype.send = function (v) {
-    /*jslint plusplus: true, ass: true */
-    var chan = wm.get(this), next, send;
-    //if (chan["[[ChannelError]]"]) return env.Promise.reject(chan["[[ChannelError]]"]);
-    if (chan["[[ChannelError]]"]) throw chan["[[ChannelError]]"];
-    while (chan["[[Channel:next:length]]"] > 0) {
-      next = channelFifoPop(chan, "next");
-      //if (next && !next.done) { next.resolve({value: v, done: false}); return env.Promise.resolve(); }
-      if (next && !next.done) return next.resolve({value: v, done: false});
-    }
-    send = channelFifoPush(chan, "send", v);
-    //if (chan["[[Channel:send:length]]"] <= chan["[[ChannelCapacity]]"]) send.resolve();
-    if (chan["[[Channel:send:length]]"] <= chan["[[ChannelCapacity]]"]) return send.resolve();
-    return send.promise;
+  Channel.prototype.next = function (iffilled) {
+    // iffilled = false
+    //   if true, returns next only if someone is sending or if the channel is filled, else null
+    // returns object or Promise<object>, a dict like {value: next value, done: if channel closed}
+    var n = this["[[ChannelPendingSend]]"][this["[[ChannelCapacity]]"]];
+    if (n) n.resolve(false);
+    n = this["[[ChannelPendingSend]]"].shift();
+    if (this["[[ChannelPendingSend]]"].length < this["[[ChannelCapacity]]"]) this["[[ChannelLength]]"] = this["[[ChannelPendingSend]]"].length;
+    if (n) return {value: n.value, done: false};
+    if (this["[[ChannelClosed]]"]) return {value: undefined, done: true};
+    if (iffilled) return null;
+    this["[[ChannelPendingNext]]"].push(n = env.Channel._magic(this["[[ChannelPendingNext]]"]));
+    return n.promise;
   };
-  Channel.prototype.next = function () {
-    /*jslint plusplus: true, ass: true */
-    var chan = wm.get(this), send;
-    while (chan["[[Channel:send:length]]"] > 0) {
-      send = channelFifoPop(chan, "send");
-      if (send && !send.done) {
-        send.resolve();
-        //return env.Promise.resolve({value: send.value, done: false});
-        return {value: send.value, done: false};
+  Channel.select = function (selector, iffilled) {
+    // selector = [chan1, chan2, chan3]
+    //   MUST be Channel instances
+    // iffilled = false
+    //   if true, returns first next only if someone is sending or if one channel is filled, else null
+    // returns object or Promise<object>, a dict link {value: first next value, done: if related channel is closed}
+
+    // Acts like `select` in go:
+    //   select {
+    //   case <-a:
+    //     fmt.Println("a");
+    //   default:
+    //     fmt.Println("default");
+    //   }
+    // equivalent in javascript:
+    //   var v = Channel.select([a], true);
+    //   if (v === null)
+    //     console.log("default")
+    //   else if (v.channel === a)
+    //     console.log("a")
+
+    function concurrentSafeNext(pos, o) {
+      var self = this, n;
+      //if ((n = this.next(true))) return n;
+      this["[[ChannelPendingNext]]"].push(n = env.Channel._magic(this["[[ChannelPendingNext]]"]));
+      n.resolve = function (va) {
+        var j = 0, m = o.cc.length;
+        function fn() {}
+        for (; j < m; ++j) if (j !== pos) {
+          o.cc[j].then(null, fn);  // catch cancel errors
+          try { o.cc[j].cancel(); } catch (_) {}
+        }
+        o.resolve({channel: self, index: pos, value: va.value, done: va.done});
+      };
+      return n.promise;
+    }
+
+    var i = 0, l = selector.length, c, v, o;
+    for (; i < l; ++i)
+      if (v = (c = selector[i]).next(true))
+        return {channel: c, index: i, value: v.value, done: v.done};
+    if (iffilled) return null;  // {channel: null, index: -1, value: undefined, done: false};
+
+    o = {cc: [], resolve: null, reject: null, promise: null};
+    o.promise = env.newPromise(function (r, j) {
+      o.resolve = r;
+      o.reject = j;
+    });
+    o.promise.cancel = function () {
+      var j = 0, m = o.cc.length;
+      function fn() {}
+      for (; j < m; ++j) {
+        o.cc[j].then(null, fn);  // catch cancel errors
+        try { o.cc[j].cancel(); } catch (_) {}
       }
-    }
-    if (chan["[[ChannelError]]"]) {
-      if (chan["[[ChannelError]]"] === CLOSED_ERROR)  // use Channel.CLOSED_ERROR ?
-        //return env.Promise.resolve({value: undefined, done: true});
-        return {value: undefined, done: true};
-      //return env.Promise.reject(chan["[[ChannelError]]"]);
-      throw chan["[[ChannelError]]"];
-    }
-    return channelFifoPush(chan, "next").promise;
+      o.reject(new Error("channel selection cancelled"));
+    };
+    for (i = 0; i < l; ++i)
+      o.cc.push(concurrentSafeNext.call(selector[i], i, o));
+    return o.promise;
   };
-  // XXX does not work correctly due to possible random native Promise resolution
-  //Channel.select = function (cases) {
-  //  // `select` blocks until one of its cases can run, then it executes that case.
-  //  // The default case in a select is run if no other case is ready.
-  //  //
-  //  //     return Channel.select([
-  //  //       [chan1, function (v) { return v.value; }],  // case 1
-  //  //       [chan2, function (v) { return v.value; }],  // case 2
-  //  //       function () { return "default"; }           // default case
-  //  //     ]);
-
-  //  // API stability level: 1 - Experimental
-  //  var i, l = cases.length, p = new Array(l), s = new Array(l), never;
-  //  function ret(v) { return v; }
-  //  function solve(fn, v) {
-  //    if (never) { return never; }
-  //    never = env.newPromise(ret);
-  //    return fn(v);
-  //  }
-  //  function reject(r) {
-  //    if (never) { return never; }
-  //    never = env.newPromise(ret);
-  //    return env.Promise.reject(r);
-  //  }
-  //  for (i = 0; i < l; i += 1) {
-  //    if (typeof cases[i] === "function") {
-  //      p[i] = env.Promise.resolve();
-  //      s[i] = env.Task.sequence([ret.bind(null, p[i]), [solve.bind(null, cases[i]), reject]]);
-  //    } else {
-  //      p[i] = cases[i][0].next();
-  //      s[i] = env.Task.sequence([ret.bind(null, p[i]), [solve.bind(null, cases[i][1]), reject]]);
-  //    }
-  //  }
-  //  env.Task.raceWinOrCancel(p);
-  //  return env.Task.race(s);
-  //};
-
   env.Channel = Channel;
   env.newChannel = function () { var c = env.Channel, o = Object.create(c.prototype); c.apply(o, arguments); return o; };
 
