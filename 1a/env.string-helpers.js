0b8ca6f5fedbca469ff626b777fe314f704b0c9d
Tristan Cavelier <t.cavelier@free.fr>
2017-02-08 16:38:17 +0100

    string-helpers: add splitLines

--- 1a/env.string-helpers.js
+++ 1a/env.string-helpers.js
@@ -2,16 +2,18 @@
 (function envStringHelpers(env) {
   "use strict";
 
-  /*! env.string-helpers.js Version 1.0.1
+  /*! env.string-helpers.js Version 1.1.0
 
-      Copyright (c) 2015-2016 Tristan Cavelier <t.cavelier@free.fr>
+      Copyright (c) 2015-2017 Tristan Cavelier <t.cavelier@free.fr>
       This program is free software. It comes without any warranty, to
       the extent permitted by applicable law. You can redistribute it
       and/or modify it under the terms of the Do What The Fuck You Want
       To Public License, Version 2, as published by Sam Hocevar. See
       http://www.wtfpl.net/ for more details. */
 
-  // provides: env.formatStringFromDict
+  // provides:
+  //   env.formatStringFromDict
+  //   env.splitLines
 
   if (env.registerLib) env.registerLib(envStringHelpers);
 
@@ -30,5 +32,41 @@
     });
   }
   env.formatStringFromDict = formatStringFromDict;
+
+  env.splitLines = function (text) {
+    //   > splitLines('ab c\n\nde fg\rkl\r\n')
+    //   ['ab c\n', '\n', 'de fg\r', 'kl\r\n']
+    // Acts like python2 `str.splitlines(keepends=True)`.
+
+    // It's also equal to the code below but 8 times faster:
+    //     var result = [];
+    //     text.replace(/(?:[^\r\n]*(?:\r\n|\r|\n)|[^\r\n]+)/g, line => result.push(line));
+    //     return result;
+
+    // Should we add these line break too ?
+    //   \v or \x0b  Line Tabulation
+    //   \f or \x0c  Form Feed
+    //   \x1c        File Separator
+    //   \x1d        Group Separator
+    //   \x1e        Record Separator
+    //   \x85        Next Line (C1 Control Code)
+    //   \u2028      Line Separator
+    //   \u2029      Paragraph Separator
+
+    // If you want to split lines without having line breaks in the
+    // result, just use:
+    //     return text.split(/(?:\r\n|\r|\n)/g);
+
+    var i = 0, l = text.length, li = 0, result = [];
+    for (; i < l; ++i) {
+      if (text[i] === "\n") result.push(text.slice(li, (li = i + 1)));
+      else if (text[i] === "\r") {
+        if (text[i + 1] === "\n") result.push(text.slice(li, (li = (++i) + 1)));
+        else result.push(text.slice(li, (li = i + 1)));
+      }
+    }
+    if (li !== i) result.push(text.slice(li));
+    return result;
+  };
 
 }(this.env));
