d3d1466a79b8499134e47e35b93debf9320b6984
Tristan Cavelier <t.cavelier@free.fr>
2017-02-22 16:24:59 +0100

    html5.node.js: fix banner

--- 1a/html5.node.js
+++ 1a/html5.node.js
@@ -3,7 +3,7 @@
 
   /*! html5.node.js Version 1.0.0
 
-      Copyright (c) 2015-2017 Tristan Cavelier <t.cavelier@free.fr>
+      Copyright (c) 2017 Tristan Cavelier <t.cavelier@free.fr>
       This program is free software. It comes without any warranty, to
       the extent permitted by applicable law. You can redistribute it
       and/or modify it under the terms of the Do What The Fuck You Want
